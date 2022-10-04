6deba54e5cce82a6bfa66d5c1459a8b4b93dd1de
Fernando Jimenez <fgjv06@gmail.com>
2022-10-04 15:39:37 -0300

    Capturing user input and comparing to random number

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let computerGuess;
+let userGuesses = [];
 
 function init() {
   computerGuess = Math.floor(Math.random() * 100 + 1);
+  console.log(computerGuess);
   document.getElementById("newGameButton").style.display = "none";
   document.getElementById("gameArea").style.display = "none";
 }
@@ -17,4 +19,20 @@ function easyMode() {
 
 function hardMode() {
   startGameView();
+}
+
+function compareGuess() {
+  const userGuess = Number(document.getElementById("inputBox").value);
+  userGuesses.push(" " + userGuess);
+  document.getElementById("guesses").innerHTML = userGuesses;
+
+  if(userGuess > computerGuess) {
+    document.getElementById("textOutput").innerHTML = "Your guess is too high";
+    document.getElementById("inputBox").value = "";
+  } else if(userGuess < computerGuess) {
+    document.getElementById("textOutput").innerHTML = "Your guess is too low";
+    document.getElementById("inputBox").value = "";
+  } else {
+    document.getElementById("textOutput").innerHTML = "Correct!";
+  }
 }

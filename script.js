79d07bda2e38fa0acfe5acd76dfbab16713bc14c
Fernando Jimenez <fgjv06@gmail.com>
2022-10-04 15:46:57 -0300

    Adding previous attempts

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let computerGuess;
 let userGuesses = [];
+let attempts = 0;
 
 function init() {
   computerGuess = Math.floor(Math.random() * 100 + 1);
@@ -25,7 +26,8 @@ function compareGuess() {
   const userGuess = Number(document.getElementById("inputBox").value);
   userGuesses.push(" " + userGuess);
   document.getElementById("guesses").innerHTML = userGuesses;
-
+  attempts++;
+  document.getElementById("attempts"). innerHTML = attempts;
   if(userGuess > computerGuess) {
     document.getElementById("textOutput").innerHTML = "Your guess is too high";
     document.getElementById("inputBox").value = "";
@@ -33,6 +35,6 @@ function compareGuess() {
     document.getElementById("textOutput").innerHTML = "Your guess is too low";
     document.getElementById("inputBox").value = "";
   } else {
-    document.getElementById("textOutput").innerHTML = "Correct!";
+    document.getElementById("textOutput").innerHTML = "Correct! You got it in " + attempts + " attempts";
   }
 }
